30c275f1de90ae5ddf9cce9e3709a36e37899390
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-30 15:12:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_key_vault_secret" "kvsecret" {
   for_each     = var.config
   name         = each.value.secretname
-  value        = var.value.secretvalue
+  value        = each.value.secretvalue
   key_vault_id = each.value.keyvault.id
 }
