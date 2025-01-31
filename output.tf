bef7cea262161920a25647bbd80c6a6a9bc79e07
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-31 21:03:18 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,9 @@ output "secret_id" {
   value = azurerm_key_vault_secret.kvsecret.id
   sensitive = true
 }
+
+
+output "name" {
+  value = azurerm_key_vault_secret.kvsecret.name
+  sensitive = true
+}
